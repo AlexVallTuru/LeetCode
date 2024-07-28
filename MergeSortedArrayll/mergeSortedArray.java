aa5f7ae8870ad21a18787d001c5a5520f3c32536
DESKTOP-BCBFLT7\Alex <alexvall24@gmail.com>
2024-07-28 15:49:14 +0200

    mergeSortedArrayll

---
+++ MergeSortedArrayll/mergeSortedArray.java
@@ -0,0 +1,31 @@
+//package MergeSortedArrayll;
+
+public class mergeSortedArray {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
+
+    public int removeDuplicates(int[] nums) {
+
+        int contador = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            for(int j = 0; j < nums.length; j++){
+
+                if(nums[i] == nums[j]){
+                    nums[i]
+                    contador++;
+                }
+            }
+
+        }
+
+        return 0;
+
+    }
+
+}
