4c06c42694196e3923e497fa9980c765dbc8a7d0
AlexVallTuru <vall_06_@hotmail.com>
2024-08-13 09:14:34 +0200

    SummaryRanges

--- SummaryRanges/summaryRanges.java
+++ SummaryRanges/summaryRanges.java
@@ -1,4 +1,4 @@
-package SummaryRanges;
+//package SummaryRanges;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +9,7 @@ public class summaryRanges {
 
         System.out.println("Hello World!");
 
-        List<String> result = summaryRangesX(new int[] { 0, 2, 4, 5, 6, 7 });
+        List<String> result = summaryRangesX(new int[] { 0, 2, 4, 5, 6, 8 });
 
         for (String apareo : result) {
             System.out.println(apareo);
@@ -24,9 +24,30 @@ public class summaryRanges {
         if (nums.length == 0) {
             return apareos;
         }
-        
-        return apareos;
 
+        int start = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // Si hay una discontinuidad
+            if (nums[i] - nums[i - 1] != 1) {
+                if (nums[i - 1] == start) {
+                    // Si el rango solo tiene un número
+                    apareos.add(Integer.toString(start));
+                } else {
+                    // Si el rango tiene más de un número
+                    apareos.add(start + "->" + nums[i - 1]);
+                }
+                start = nums[i]; // Nuevo rango empieza desde el número actual
+            }
+        }
+
+        // Añadir el último rango después de terminar el bucle
+        if (nums[nums.length - 1] == start) {
+            apareos.add(Integer.toString(start));
+        } else {
+            apareos.add(start + "->" + nums[nums.length - 1]);
+        } 
+        return apareos;
     }
 
 }
