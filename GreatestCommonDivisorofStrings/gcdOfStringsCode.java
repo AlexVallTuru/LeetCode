8ce6fcf17123b8025fadb797e70affb7f0182444
AlexVallTuru <vall_06_@hotmail.com>
2024-09-30 18:01:37 +0200

    gcd

--- GreatestCommonDivisorofStrings/gcdOfStringsCode.java
+++ GreatestCommonDivisorofStrings/gcdOfStringsCode.java
@@ -14,24 +14,23 @@ public class gcdOfStringsCode {
 
     public static String gcdOfStrings(String str1, String str2) {
 
-        if (str1.equals("") || str2.equals("")) {
+        // Algoritmo de Euclides o la operación del máximo común divisor (GCD) 
+
+        if(!(str1 + str2).equals(str2 + str1)){
             return "";
         }
 
-        if (str1.equals(str2)) {
-            return str1;
-        }
-
-        int len1 = str1.length();
-        int len2 = str2.length();
-
-        if (len1 > len2) {
-
-        } else {
-            
-        }
-
-        return "";
+        return str1.substring(0, gcd(str1.length(), str2.length()));
     }
 
+    public static int gcd(int a, int b){
+
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
 }
